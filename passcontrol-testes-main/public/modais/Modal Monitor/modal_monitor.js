464a81958ff5c1fb7902d8d705f2a567ed3016b4
SesperSes <Sesperses@gmail.com>
2025-04-01 11:41:34 -0400

    amem

--- passcontrol-testes-main/public/modais/Modal Monitor/modal_monitor.js
+++
@@ -1,6 +0,0 @@
-const buttonAbrir = document.querySelector(".open");
-const modalContainer = document.querySelector(".modal-container");
-
-buttonAbrir.addEventListener("click", () => {
-    modalContainer.classList.add("show");
-});
